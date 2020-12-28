4c0b190befe736efd9d01c56ea7d1dd2082dfa73
manassingi <70809272+manassingi@users.noreply.github.com>
2020-12-28 23:03:04 +0530

    Add files via upload

---
+++ slingshort.js
@@ -0,0 +1,40 @@
+class Slingshot{
+    
+        constructor(body, anchor){
+            var options = {
+                bodyA: body,
+                pointB: anchor,
+                stiffness: 0.04,
+                length: 10
+            }
+            this.pointB = anchor
+            this.bodyA=body
+           
+            this.slingshort = Constraint.create(options);
+           // this.colour(random(0,225),random(0,225),random(0,225))
+            World.add(world,this.slingshort);
+        }
+         attach(body){
+            this.slingshort.bodyA=body;
+          
+         }
+    
+         fly(){
+            
+            this.slingshort.bodyA=null;
+         }
+        
+             
+        display(){
+            if(this.slingshort.bodyA){
+    
+            
+             var pointA = this.bodyA.position;
+             var pointB = this.pointB;
+             strokeWeight(0);
+             line(220,420, 220,420);
+            }
+        }
+       
+    }
+    
